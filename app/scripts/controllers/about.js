797ac128ed5e2f1e1e41184ea348ce5d0d4aa123
spunkcum <philip_kierkegaard@hotmail.com>
2018-03-17 13:09:56 +0100

    Created project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mercedesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mercedesApp
+ */
+angular.module('mercedesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mercedesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mercedesApp
+ */
+angular.module('mercedesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
