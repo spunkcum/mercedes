7ebace70b26b3899fe8f1528ae34bab2dcd9ad57
AlexKotsc <alexkotsc@gmail.com>
2018-06-02 20:23:52 +0200

    Added video and titles to gallery page and reintroduced it in the menu.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -12,7 +12,8 @@
 
  function GalleryController(){
  	document.title = "Mercedes-Benz Classic Car Race - Galleri";
- 	document.getElementById("pageHeader").style.display = "none";
+ 	document.getElementById("pageHeader").innerHTML = "Tak for sidst.";
+ 	document.getElementById("pageHeader").style.display = "block";
 
  	lightGallery(document.getElementById('lightgallery')); 
 
